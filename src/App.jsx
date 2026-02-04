6bffd9596d45f35c0968676fa8171d22d8caf8ea
John Abuel <johnchristiannino.abuel@g.msuiit.edu.ph>
2026-02-04 23:19:55 +0800

    Finished getPokemon function

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
+const Button = ({ onClick, text }) => {
+  return (
+    <button className="w-30 h-15 border-4 bg-blue-200 flex items-center justify-center" onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div className='App'>
-        <PokemonCard />
+        <PokemonCard ButtonComponent={Button} />
       </div>
     </>
   )
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,45 @@
 import React from "react";
+import axios from 'axios';
+import _ from 'lodash';
+import { useState } from "react";
 
-const PokemonCard = () => {
+const PokemonCard = ({ ButtonComponent }) => {
+    const [isLoading, setLoad] = useState(true)
+    const [pokemonName, setPokemonName] = useState('')
+    const [pokemonSpriteUrl, setPokemonSprite] = useState('')
+    const [pokemonTypes, setPokemonTypes] = useState([])
+    const [pokemonMoves, setPokemonMoves] = useState([])
+
+    const loadRandomPokemonCard = async () => {
+        setLoad(!isLoading)
+    }
+
+    const randomPokemonId = Math.floor(Math.random() * 151) + 1;
+
+    const getPokemon = async () => {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}/`)
+            if (!response.ok) {
+                throw new Error('Error loading pokemon.');
+            }
+            const data = await response.json();
+            alert('Fetching a new Pokemon!');
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.error('Error fetching data:', error)
+        }
+
+    }
+
+
     return (
-        <div className="w-60 h-60 border-4 bg-red-900 flex items-center justify-center">
-            Pokemon Card yawa
-        </div>
+        <>
+            <ButtonComponent onClick={getPokemon} text="Get Pokemon">Get Pokemon</ButtonComponent>
+            <div className="w-60 h-60 border-4 bg-red-900 flex items-center justify-center">
+                Pokemon Card
+            </div>
+        </>
     )
 }
 export default PokemonCard
